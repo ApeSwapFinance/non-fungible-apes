1acda8f1ccd6729373e27ab732531dcf8a599fc0
defifofum <defifofum0@gmail.com>
2021-10-26 22:15:10 -0400

    refactor: Update deploy config

--- migrations/2_deploy_nfa.js
+++ migrations/2_deploy_nfa.js
@@ -11,23 +11,31 @@ module.exports = async function (deployer, network, accounts) {
   await deployer.deploy(NonFungibleApes, name, symbol, baseTokenURI, { from: deployerAccount });
   const nfaContract = await NonFungibleApes.at(NonFungibleApes.address);
 
+  let adminHasAdminRole;
+  let adminHasMinterRole;
+  let deployerHasAdminRole;
+  let deployerHasMinterRole;
+
+
   /**
    * Setup Roles
    */
-  // Transfer ownership to admin
-  const DEFAULT_ADMIN_ROLE = await nfaContract.DEFAULT_ADMIN_ROLE();
-  const MINTER_ROLE = await nfaContract.MINTER_ROLE();
-  // Grant admin proper roles
-  await nfaContract.grantRole(DEFAULT_ADMIN_ROLE, admin, { from: deployerAccount });
-  await nfaContract.grantRole(MINTER_ROLE, admin, { from: deployerAccount });
-  // Renounce admin from deployer
-  await nfaContract.renounceRole(DEFAULT_ADMIN_ROLE, deployerAccount, { from: deployerAccount });
-  await nfaContract.renounceRole(MINTER_ROLE, deployerAccount, { from: deployerAccount });
-  // Verify results
-  const adminHasAdminRole = await nfaContract.hasRole(DEFAULT_ADMIN_ROLE, admin, { from: deployerAccount });
-  const adminHasMinterRole = await nfaContract.hasRole(MINTER_ROLE, admin, { from: deployerAccount });
-  const deployerHasAdminRole = await nfaContract.hasRole(DEFAULT_ADMIN_ROLE, deployerAccount, { from: deployerAccount });
-  const deployerHasMinterRole = await nfaContract.hasRole(MINTER_ROLE, deployerAccount, { from: deployerAccount });
+  if (admin) {
+    // Transfer ownership to admin
+    const DEFAULT_ADMIN_ROLE = await nfaContract.DEFAULT_ADMIN_ROLE();
+    const MINTER_ROLE = await nfaContract.MINTER_ROLE();
+    // Grant admin proper roles
+    await nfaContract.grantRole(DEFAULT_ADMIN_ROLE, admin, { from: deployerAccount });
+    await nfaContract.grantRole(MINTER_ROLE, admin, { from: deployerAccount });
+    // Renounce admin from deployer
+    await nfaContract.renounceRole(DEFAULT_ADMIN_ROLE, deployerAccount, { from: deployerAccount });
+    await nfaContract.renounceRole(MINTER_ROLE, deployerAccount, { from: deployerAccount });
+    // Verify results
+    const adminHasAdminRole = await nfaContract.hasRole(DEFAULT_ADMIN_ROLE, admin, { from: deployerAccount });
+    const adminHasMinterRole = await nfaContract.hasRole(MINTER_ROLE, admin, { from: deployerAccount });
+    const deployerHasAdminRole = await nfaContract.hasRole(DEFAULT_ADMIN_ROLE, deployerAccount, { from: deployerAccount });
+    const deployerHasMinterRole = await nfaContract.hasRole(MINTER_ROLE, deployerAccount, { from: deployerAccount });
+  }
 
   // Log/verify results
   console.dir({
